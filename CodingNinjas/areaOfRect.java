c5db534b9ac9ac8dff07e9ea1bd30af250722d06
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-08-07 10:26:33 +0530

    Create areaOfRect.java

---
+++ CodingNinjas/areaOfRect.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		int area = l*b;
+		System.out.println(area);
+
+		
+	}
+}
