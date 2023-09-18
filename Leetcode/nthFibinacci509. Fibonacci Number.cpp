db64177a80f62d64d3a05411e169d61a8b176768
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-09-18 18:26:54 +0530

    Create nthFibinacci509. Fibonacci Number.cpp

---
+++ Leetcode/nthFibinacci509. Fibonacci Number.cpp
@@ -0,0 +1,40 @@
+int rec(int n) {
+    if(n==0) return n;
+    if(n==1) return n;
+    int temp = rec(n-1)+rec(n-2);
+    return temp;
+}
+class Solution {
+public:
+    int fib(int n) {
+        int ans = rec(n);
+        return ans;
+
+        
+    }
+};
+
+
+/*
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+*/
