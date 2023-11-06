5162ae58b2fddc617085e125b324132a100bff84
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-11-06 14:36:50 +0530

    Create InsertionSort_CN.java

---
+++ DAYWISE/06112023/InsertionSort_CN.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+
+        for(int i = 1 ; i < size ; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
