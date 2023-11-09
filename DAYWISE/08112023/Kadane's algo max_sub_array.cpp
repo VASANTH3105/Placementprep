58531b77df0db4ef69a92d1360955ed28553513f
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-11-09 12:18:04 +0530

    Create Kadane's algo max_sub_array.cpp

---
+++ DAYWISE/08112023/Kadane's algo max_sub_array.cpp
@@ -0,0 +1,21 @@
+Leet code
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int max_so_far = nums[0];
+        int max_end_here = 0;
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            max_end_here = max_end_here + nums[i];
+            if(max_so_far < max_end_here) {
+                max_so_far = max_end_here;
+            }
+
+            if(max_end_here < 0) {
+                max_end_here = 0;
+            }
+        }
+        return max_so_far;
+    }
+};
