7217e4ef1b0fd848b1e19ee50b28f150b359e7a8
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-11-09 12:20:24 +0530

    Update Kadane's algo max_sub_array.cpp

--- DAYWISE/08112023/Kadane's algo max_sub_array.cpp
+++ DAYWISE/08112023/Kadane's algo max_sub_array.cpp
@@ -19,3 +19,26 @@ public:
         return max_so_far;
     }
 };
+
+
+GFG pratice
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int max_so_far = nums[0];
+        int max_end_here = 0;
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            max_end_here = max_end_here + nums[i];
+            if(max_so_far < max_end_here) {
+                max_so_far = max_end_here;
+            }
+
+            if(max_end_here < 0) {
+                max_end_here = 0;
+            }
+        }
+        return max_so_far;
+    }
+};
