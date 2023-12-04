a2847047bc67a2e69a6954faf9c92360680d9074
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-12-04 15:15:11 +0530

    Create searchelementsinLL.java

---
+++ DAYWISE/04122023LL/searchelementsinLL.java
@@ -0,0 +1,43 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution
+{
+    public static int searchInLinkedList(Node head, int k)
+    {
+        // Write Your Code Here.
+        Node currentNode = head;
+        while(currentNode!=null) {
+          if(currentNode.data == k) return 1;
+          currentNode = currentNode.next;
+        }
+      return 0;
+    }
+}
