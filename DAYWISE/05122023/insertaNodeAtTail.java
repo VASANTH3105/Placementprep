5955dbbeccdc4c74977fda11bd696f53e47eab65
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2023-12-05 23:12:21 +0530

    Update insertaNodeAtTail.java

--- DAYWISE/05122023/insertaNodeAtTail.java
+++ DAYWISE/05122023/insertaNodeAtTail.java
@@ -1,1 +1,63 @@
+/****************************************************************
 
+ Following is the linkedList class structure:
+
+ class Node
+ {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+
+    public static Node insertEnd(Node head, int k) {
+
+        // Write Your Code Here.
+
+        Node newNode=new Node(k);
+
+        if (head==null) {
+
+            head=newNode;
+
+        }
+
+        else{
+
+           Node temp=head;  
+
+           while(temp.next!=null){
+
+                temp=temp.next;
+
+            }
+
+            temp.next=newNode; 
+
+        } 
+
+        return head;
+
+    }
+
+}
