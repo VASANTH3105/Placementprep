582956b5ce5f974bf8f0c53a9ea54d55fd175b31
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-01-27 00:15:27 +0530

    Create At least two greater elements.cpp

---
+++ 2024/At least two greater elements.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    vector<int> findElements(int a[], int n)
+    {
+        // Your code goes here 
+        sort(a, a+n);
+        int k = n-2;
+        vector<int> result;
+        for(int i = 0 ; i < k ; i++) {
+            result.push_back(a[i]);
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        Solution ob;
+        vector <int> res = ob.findElements(a,n);
+        
+        for(int i=0;i<res.size();i++)
+        cout<<res[i]<<" ";
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
