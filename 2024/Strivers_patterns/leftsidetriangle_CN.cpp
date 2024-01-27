020690b5e9d645505b38b1fa4e653a5ac5025b2d
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-01-27 22:40:21 +0530

    Update leftsidetriangle_CN.cpp

--- 2024/Strivers_patterns/leftsidetriangle_CN.cpp
+++ 2024/Strivers_patterns/leftsidetriangle_CN.cpp
@@ -7,3 +7,20 @@ void nForest(int n) {
 		cout<<endl;
 	}
 }
+
+
+_______________________
+
+1
+12
+123
+1234
+void nTriangle(int n) {
+	// Write your code here
+	for(int i = 1 ; i <= n ; i++) {
+		for(int j = 1 ; j <= i ; j++) {
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+}
