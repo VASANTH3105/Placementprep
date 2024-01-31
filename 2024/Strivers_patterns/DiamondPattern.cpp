fd56c51ba79c323dbd6b877b2442bb1fdb7815b0
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-01-31 22:32:26 +0530

    Create DiamondPattern.cpp

---
+++ 2024/Strivers_patterns/DiamondPattern.cpp
@@ -0,0 +1,27 @@
+void nStarDiamond(int n) {
+    // Write your code here.
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n - i - 1 ; j++) {
+            cout<<" ";
+        }
+        for(int j = 0 ; j < 2*i+1 ; j++) {
+            cout<<"*";
+        }
+        for(int j = 0 ; j < n - i - 1; j++) {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < i ; j++) {
+            cout<< " ";
+        }
+        for(int j = 0 ; j < 2* n  -(2 * i +1) ; j++) {
+            cout<<"*";
+        }
+        for(int j = 0 ; j < i ; j++) {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
