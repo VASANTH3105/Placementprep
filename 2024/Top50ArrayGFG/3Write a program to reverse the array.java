d5b3da395423feb943bb450c2aaae6135198dd98
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-02-05 22:07:25 +0530

    Create 3Write a program to reverse the array.java

---
+++ 2024/Top50ArrayGFG/3Write a program to reverse the array.java
@@ -0,0 +1,17 @@
+
+
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        String rev = new StringBuilder(str).reverse().toString();
+        return rev;
+        
+    }
+}
