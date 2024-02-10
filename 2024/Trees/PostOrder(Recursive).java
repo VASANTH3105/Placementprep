031b0876b20bef2181063e6a34f1c56711bf6403
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-02-10 22:02:59 +0530

    Create PostOrder(Recursive).java

---
+++ 2024/Trees/PostOrder(Recursive).java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        return postorderTraversal(root, list);
+        
+    }
+    private List<Integer> postorderTraversal(TreeNode root, List<Integer> list) {
+        if( root != null) {
+            postorderTraversal(root.left, list);
+            postorderTraversal(root.right, list);
+            list.add(root.val);
+        }
+        return list;
+    }
+}
