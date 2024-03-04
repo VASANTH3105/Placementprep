8d990a57abfa5cc8bc7d4c14d2c11a74a8b84511
VASANTH S <122204153+VASANTH3105@users.noreply.github.com>
2024-03-04 15:32:31 +0530

    Create Reverse array in groups.java

---
+++ 2024/600CheatSheet/Arrays/Reverse array in groups.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        
+        //while testcases exist
+        while(t-->0){
+            
+            String inputLine1[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine1[0]);
+            int k = Integer.parseInt(inputLine1[1]);
+            
+            ArrayList<Integer> arr = new ArrayList<>();            
+            String inputLine2[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr.add(Integer.parseInt(inputLine2[i]));
+            }
+            
+            Solution obj = new Solution();
+            obj.reverseInGroups(arr, n, k);
+            
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr.get(i) + " ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    //Function to reverse every sub-array group of size k.
+    void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+        // code here
+        // 1, 2, 3, 4, 5
+        // 3, 2, 1, 4, 5
+        for(int i = 0 ; i < n ; i += k) {
+            reverseArray(i, Math.min(n-1, i+k-1), arr); // 0 -> 3 then 4, 2
+        }
+        // 1,2,3,4
+    }
+    
+    public static void reverseArray(int start , int end, ArrayList<Integer>arr) {
+        while(start < end) {
+            int temp = arr.get(start);
+            arr.set(start, arr.get(end));
+            arr.set(end, temp);
+            start++;
+            end--;
+        }
+    }
+}
